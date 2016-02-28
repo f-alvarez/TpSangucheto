88a2991c5b6951957cfa5c8a26f044fe4e70d442
f-alvarez <floor.alvarez@gmail.com>
2016-02-28 18:22:42 -0300

    Creacion de clases

---
+++ src/tallerweb/springmvc/model/Ingrediente.java
@@ -0,0 +1,28 @@
+package tallerweb.springmvc.model;
+
+public class Ingrediente {
+	
+	  private String nombre;
+	    private Double precio;
+	    private TipoIngrediente tipo;
+	    
+	    public String getNombre() {
+	        return nombre;
+	    }
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+	    public Double getPrecio() {
+	        return precio;
+	    }
+	    public void setPrecio(Double precio) {
+	        this.precio = precio;
+	    }
+	    public TipoIngrediente getTipo() {
+	        return tipo;
+	    }
+	    public void setTipo(TipoIngrediente tipo) {
+	        this.tipo = tipo;
+	    }
+
+}
